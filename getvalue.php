d3376731a1b8e260a098b41de2fe94b0bd40f089
Diego Barreiro <diego.barreiro.perez@gmail.com>
2018-07-19 15:40:16 +0200

    FIX: Change Request Params

--- getvalue.php
+++ getvalue.php
@@ -1,17 +1,17 @@
 <?php
 
-parse_str($_SERVER['QUERY_STRING'], $POST);
-if ($_SERVER['REQUEST_METHOD'] != "POST" || !isset($POST['tag'])) {
+$file = "database.txt";
+
+if ($_SERVER['REQUEST_METHOD'] != "POST" || !isset($_REQUEST['tag'])) {
     die("Not Allowed");
 }
-$tag = trim($POST['tag']);
+$tag = trim($_REQUEST['tag']);
 
-$file = "database.txt";
 $f = fopen($file, 'r');
 $data = json_decode(fgets($f), true);
 fclose($f);
 
 $result = array("VALUE", $tag, $data[$tag]);
-echo json_encode($result); 
+echo json_encode($result);
 
 ?>
